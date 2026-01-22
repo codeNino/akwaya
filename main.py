98cafe21c7bfc323824cc02b3ca068aba1c8b336
codenino <samuelfadunsin@gmail.com>
2026-01-22 14:03:40 +0100

    added env var validator

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import asyncio
 
+from internal.config.secret import validate_environment
 from internal.domain.scraper import WebCawler
 from internal.config.paths_config import (FUNNEL_CONFIG_PATH,
         RAW_PROSPECTIVE_WHITELABELS_PATH,
         RAW_PROSPECTIVE_INDIVIDUALS_PATH)
+
+validate_environment(
+    ["SERPER_API_KEY", "GOOGLE_API_KEY", "OPENAI_KEY"]
+)
+    
 
 web_cawler = WebCawler()
 
