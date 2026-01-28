391bec60274f546bbf7fe697550852cbaffdf1bf
codenino <samuelfadunsin@gmail.com>
2026-01-28 16:54:27 +0100

    created service method and controller to handle feedback from retell

--- internal/domain/service.py
+++ internal/domain/service.py
@@ -1,3 +1,9 @@
+from typing import Dict, List
+from internal.domain.common.dto import CustomCallAnalysisData
+
+from internal.config.paths_config import ARTIFACTS_DIR
+from internal.utils.loader import export_to_json
+
 from internal.domain.pipeline.augmentation import trigger_leads_information_augmentation
 from internal.domain.pipeline.ingestion import trigger_leads_sourcing
 from internal.config.paths_config import (LEADS_SOURCED_PATH, LEADS_AUGMENTED_PATH)
@@ -14,3 +20,10 @@ def run_leads_acquisition_pipeline(query: str):
         LEADS_SOURCED_PATH,
         LEADS_AUGMENTED_PATH
     )    
+
+
+
+def update_leads_with_feedback(user_id: str, data: CustomCallAnalysisData):
+    # export_to_json(data, ARTIFACTS_DIR / f"feedback_about_lead_{user_id}.json")
+    pass
+    
--- server/controller.py
+++ server/controller.py
@@ -1,11 +1,43 @@
-from fastapi import (APIRouter, BackgroundTasks)
+from fastapi import (APIRouter, BackgroundTasks, Request)
 from fastapi.responses import JSONResponse
 from .dto import PipelineRequest
-from internal.domain.service import run_leads_acquisition_pipeline
+
+from internal.domain.service import (
+    run_leads_acquisition_pipeline,
+    update_leads_with_feedback
+)
+from internal.utils.logger import AppLogger
+
+controller_logger = AppLogger("server.controller")()
 
 router = APIRouter(prefix="/api/v1")
 
-@router.post("/pipeline")
-def pipeline(request: PipelineRequest, background_tasks: BackgroundTasks):
+@router.post("/leads/pipeline")
+def acquisition_pipeline(request: PipelineRequest, background_tasks: BackgroundTasks):
     background_tasks.add_task(run_leads_acquisition_pipeline, request.query)
     return JSONResponse({"message": "Pipeline triggered successfully"})
+
+@router.post("/webhook/retell_feedback")
+async def retell_cold_call_feedback(request: Request, background_tasks: BackgroundTasks):
+    try:
+        payload = await request.json()
+        controller_logger.info(f"Received feedback from cold call :: {payload.get('event')}")
+        if payload.get("event") == "call_analyzed":
+            prospect_id = payload.get("call", {}).get("metadata", {}).get("prospect_id")
+            if not prospect_id:
+                controller_logger.info("No prospect_id found in payload")
+                return JSONResponse({"message": "No prospect_id found in call metadata"})
+            call_summary = payload.get("call", {}).get("call_analysis", {}).get("call_summary", "")
+            call_recording_url = payload.get("call", {}).get("recording_url", "")
+            custom_analysis_data = payload.get("call", {}).get("call_analysis", {}).get("custom_analysis", {})
+            background_tasks.add_task(update_leads_with_feedback, prospect_id, 
+            {
+                "call_summary": call_summary,
+                "call_recording_url": call_recording_url,
+                "is_qualified_lead": custom_analysis_data.get("qualified_lead", False),
+                "is_relevant_industry": custom_analysis_data.get("relevant_industry", False),
+            })
+        return JSONResponse({"message": "Feedback Received"})
+    except Exception as e:
+        controller_logger.error(f"Error processing feedback from cold call: {e}")
+        return JSONResponse({"message": "Error processing feedback from cold call"})
